b018e1977cb8d3a742c37552eac9985574e02df8
Eloque <mark@ridersoft.net>
2026-03-01 17:45:59 +0100

    Work on per voice instructions

--- boson.py
+++ boson.py
@@ -1,23 +1,43 @@
 # boson.py
-import time
+import shutil
+import sys
+from pathlib import Path
 
+ROOT = Path(__file__).resolve().parent
+HIGGS_ROOT = ROOT / "faster-higgs-audio"
+
+sys.path.insert(0, str(HIGGS_ROOT))
+
+import argparse
 import warnings
 import chunker
 import torch
-
 import soundfile as sf
 
 from boson_multimodal.data_types import ChatMLSample, Message
 from boson_multimodal.audio_processing.higgs_audio_tokenizer import load_higgs_audio_tokenizer
 from examples.generation import HiggsAudioModelClient, prepare_generation_context, prepare_chunk_text, normalize_chinese_punctuation
+# from generation import prepare_generation_context
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 warnings.filterwarnings("ignore", category=UserWarning)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
+
 # Model and tokenizer names
 MODEL_PATH = "bosonai/higgs-audio-v2-generation-3B-base"
 TOKENIZER_PATH = "bosonai/higgs-audio-v2-tokenizer"
+
+def sync_voice_prompts():
+    root = Path(__file__).resolve().parent
+    src = root / "voices"
+    dst = root / "faster-higgs-audio" / "examples" / "voice_prompts"
+
+    dst.mkdir(parents=True, exist_ok=True)
+
+    for p in src.iterdir():
+        if p.is_file():
+            shutil.copy2(p, dst / p.name)
 
 def get_device(device_arg=None):
     """Determine the best device to use."""
@@ -38,6 +58,47 @@ def get_device(device_arg=None):
         return device_arg
 
 
+def initialize_synthesization():
+
+    device = get_device("auto")
+    device_id = None if device == "cpu" else int(device.split(":")[-1])
+
+    # To prevent multiple loadings, run this once
+    # model_client = HiggsAudioModelClient(
+    #     model_path="bosonai/higgs-audio-v2-generation-3B-base",
+    #     audio_tokenizer="bosonai/higgs-audio-v2-tokenizer",
+    #     device_id=device_id,
+    #     max_new_tokens=4096, # $4096 / 8,
+    #     use_static_kv_cache=False,
+    #     use_quantization=True,
+    #     quantization_bits=4,
+    # )
+
+    # To prevent multiple loadings, run this once
+    model_client = HiggsAudioModelClient(
+        model_path="bosonai/higgs-audio-v2-generation-3B-base",
+        audio_tokenizer="bosonai/higgs-audio-v2-tokenizer",
+        device_id=device_id,
+        max_new_tokens=378, # $4096 / 8,
+        use_static_kv_cache=False,
+        use_quantization=True,
+        quantization_bits=8,
+    )
+
+    audio_tokenizer = load_higgs_audio_tokenizer("bosonai/higgs-audio-v2-tokenizer", device=device)
+
+    return model_client, audio_tokenizer
+
+def get_generation_messages(voice):
+
+    messages, audio_ids = prepare_generation_context(
+        scene_prompt="",
+        ref_audio=voice,
+        ref_audio_in_system_message=True,
+        audio_tokenizer=audio_tokenizer,
+        speaker_tags=speaker_tags,
+    )
+
 def main():
     parser = argparse.ArgumentParser(
         description="Boson – Vocalis audio generation using HiggsAudio model"
@@ -45,7 +106,9 @@ def main():
 
     parser.add_argument(
         "voice",
-        help="Voice sample name to use (e.g. 'voice', 'chris', 'narrator_dark')"
+        help="Voice sample name to use (e.g. 'voice', 'chris', 'narrator_dark')",
+        nargs = "?",  # makes it optional
+        default = None,  # default value if not provided
     )
 
     args = parser.parse_args()
@@ -56,13 +119,15 @@ def main():
         "Take a steady breath. The cold waits for no one.",
 
         # Follow-ups (generate separately, appended after anchor in playback)
-        "The air is thin, and it settles softly in your lungs.",
-        "Each step carries weight, and the mountain remembers them all.",
-        "Frost gathers along the edges of silence.",
-        "Somewhere in the distance, something is watching.",
-        "The wind moves through stone and bone alike.",
-        "The world is vast, indifferent, and older than your fear."
+        # "The air is thin, and it settles softly in your lungs.",
+        # "Each step carries weight, and the mountain remembers them all.",
+        # "Frost gathers along the edges of silence.",
+        # "Somewhere in the distance, something is watching.",
+        # "The wind moves through stone and bone alike.",
+        # "The world is vast, indifferent, and older than your fear."
     ]
+
+    sync_voice_prompts()
 
     print(f"Using voice sample: {voice}")
 
@@ -75,21 +140,22 @@ def main():
 
     chunked_text = chunker.chunk_text(text)
 
-    device = "cuda"
-    audio_tokenizer = load_higgs_audio_tokenizer("bosonai/higgs-audio-v2-tokenizer", device=device)
-
     device = get_device("auto")
     device_id = None if device == "cpu" else int(device.split(":")[-1])
 
-    model_client = HiggsAudioModelClient(
-        model_path="bosonai/higgs-audio-v2-generation-3B-base",
-        audio_tokenizer="bosonai/higgs-audio-v2-tokenizer",
-        device_id=device_id,
-        max_new_tokens=4096,
-        use_static_kv_cache=False,
-        use_quantization=True,
-        quantization_bits=4,
-    )
+    audio_tokenizer = load_higgs_audio_tokenizer("bosonai/higgs-audio-v2-tokenizer", device=device)
+
+    # model_client = HiggsAudioModelClient(
+    #     model_path="bosonai/higgs-audio-v2-generation-3B-base",
+    #     audio_tokenizer="bosonai/higgs-audio-v2-tokenizer",
+    #     device_id=device_id,
+    #     max_new_tokens=4096,
+    #     use_static_kv_cache=False,
+    #     use_quantization=True,
+    #     quantization_bits=4,
+    # )
+
+    model_client = get_model_client()
 
     speaker_tags = []
 
@@ -101,21 +167,23 @@ def main():
         speaker_tags=speaker_tags,
     )
 
-    # messages[1] = Message(
-    #     role="system",
-    #     content=(
-    #         # "You are a deep, sonorous narrator. "
-    #         # "Your voice is cold, restrained, and heavy with foreboding. "
-    #         # "Each sentence carries weight. "
-    #         # "Pause slightly between phrases. "
-    #         # "Emotion is subtle but unmistakable."
-    #         "You are a low, warm narrator."
-    #         "Speech is steady, controlled, and deliberate."
-    #         "Emotion is restrained."
-    #         "Consonants are soft."
-    #         "Pacing is moderate."
-    #     ),
-    # )
+    # if voice is None:
+    #
+    #     messages[0] = Message(
+    #         role="system",
+    #         content=(
+    #             # "You are a deep, sonorous narrator. "
+    #             # "Your voice is cold, restrained, and heavy with foreboding. "
+    #             # "Each sentence carries weight. "
+    #             # "Pause slightly between phrases. "
+    #             # "Emotion is subtle but unmistakable."
+    #             "You are a low, warm narrator."
+    #             "Speech is steady, controlled, and deliberate."
+    #             "Emotion is restrained."
+    #             "Consonants are soft."
+    #             "Pacing is moderate."
+    #         ),
+    #     )
 
     concat_wv, sr, text_output = model_client.generate(
         messages=messages,
@@ -131,8 +199,7 @@ def main():
     )
 
     sf.write("scen0.wav", concat_wv, sr)
-    end_time = time.time()
-    print(f"[Vocalis] Total runtime: {end_time - start_time:.2f} seconds")
+
 
 if __name__ == "__main__":
     main()
--- worker.py
+++ worker.py
@@ -1,8 +1,17 @@
 # Create the folders and synthesize the audio.
 import json
 import os
+
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True,max_split_size_mb:128"
+
 from enum import Enum, auto
-from typing import Any
+
+import chunker
+from boson import initialize_synthesization, sync_voice_prompts
+from examples.generation import prepare_generation_context
+import torch
+
+import soundfile as sf
 
 class PageType(Enum):
     TITLE = auto()
@@ -10,16 +19,112 @@ class PageType(Enum):
     CONTINUED_SCENARIO = auto()
     UNKNOWN = auto()
 
-def synthesize_audio(text, voice_sample, filename):
+def synthesize_audio(model_client, tokenizer, text, voice_sample, filename):
 
-    # touch the filename, so the file exists
-    with open(filename, "w") as f:
-        f.write("")
+    if voice_sample == "None":
+        voice_sample = None
 
+    chunked_text = chunker.chunk_text(text)
+
+    style = voice_sample["style"]
+
+    # style = (
+    #     "You are a deep, sonorous narrator. "
+    #     "Voice is cold, restrained, heavy with foreboding. "
+    #     "Pause before key revelations. "
+    #     "Emphasize important words strongly. "
+    #     "Intensity rises on threats and stakes. "
+    #     "Keep pacing deliberate."
+    # )
+
+    messages, audio_ids = prepare_generation_context(
+        scene_prompt=style,
+        ref_audio=voice_sample["name"],
+        ref_audio_in_system_message=True,
+        audio_tokenizer=tokenizer,
+        speaker_tags=[],
+    )
+
+    # was .65
+    temperature = 0.75
+    top_k = 80
+    top_p = 0.97
+    ras_win_len = 4 + 20
+    ras_win_max_num_repeat = 3 + 1
+    generation_chunk_buffer_size = 2
+
+    temperature = 0.75
+    top_k = 70
+    top_p = 0.95
+    ras_win_len = 32
+    ras_win_max_num_repeat = 4
+    generation_chunk_buffer_size = 1
+
+    temperature = voice_sample["temperature"]
+    top_k = voice_sample["top_k"]
+    top_p = voice_sample["top_p"]
+
+    concat_wv, sr, text_output = model_client.generate(
+        messages=messages,
+        audio_ids=audio_ids,
+        chunked_text=chunked_text,
+        generation_chunk_buffer_size=generation_chunk_buffer_size,
+        temperature=temperature,
+        top_k=top_k,
+        top_p=top_p,
+        ras_win_len=ras_win_len,
+        ras_win_max_num_repeat=ras_win_max_num_repeat,
+        seed=1001,
+    )
+
+    sf.write(filename, concat_wv, sr)
+    torch.cuda.empty_cache()
 
 def main():
 
-    voices = ["victor", "broom_salesman"]
+    # Prepare the model client and tokenizer for synthesization
+    sync_voice_prompts()
+    model_client, tokenizer = initialize_synthesization()
+
+    voices = ["victor", "librarian", "sherlock", "slayer",
+              "first", "en_man", "en_woman", "broom_salesman",
+              "belinda", "morgan", "wick"]
+
+    voices = ["victor", "first", "librarian"]
+
+    voices = []
+
+    style = (
+        "Narrate with controlled intensity."
+        "Maintain steady pacing."
+        "When a sentence is short and declarative, slow slightly and add weight."
+        "Pause briefly before final decisive statements. "
+        "Avoid neutral tone on climactic lines."
+    )
+
+    voice = { "name": "fred",
+              "temperature": 0.70,
+              "top_k": 60,
+              "top_p": 0.95,
+              "style": style}
+
+    voices.append(voice)
+
+    style = (
+        "Narrate with controlled intensity."
+        "Maintain steady pacing."
+        "When a sentence is short and declarative, slow slightly and add weight."
+        "Pause briefly before final decisive statements. "
+        "Avoid neutral tone on climactic lines."
+    )
+
+    voice = { "name": "victor",
+              "temperature": 0.85,
+              "top_k": 80,
+              "top_p": 0.95,
+              "style": style}
+
+    # voices.append(voice)
 
     input_file = "./input/scenarios/book.json"
     book = json.load(open(input_file))
@@ -29,76 +134,91 @@ def main():
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    for entry in book:
-        # convert the entry type to the PageType enum
-        page_type = PageType[entry["type"]]
+    for entry in book[1:]:
 
-        match page_type:
-            case PageType.TITLE:
-                print(f"Title: {entry['title']}")
+        try:
 
-            case PageType.SCENARIO:
-                print(f"Scenario: {entry['number']} {entry['title']}")
+            # convert the entry type to the PageType enum
+            page_type = PageType[entry["type"]]
 
-                # pad number to be 3 digits
-                number = str(entry['number']).zfill(3)
+            match page_type:
+                case PageType.TITLE:
+                    print(f"Title: {entry['title']}")
+                    continue
 
-                # Create a folder for the scenario
-                scenario_folder = os.path.join(output_folder, f"{number} - {entry['title']}")
-                if not os.path.exists(scenario_folder):
-                    os.makedirs(scenario_folder)
+                case PageType.SCENARIO:
+                    print(f"Scenario: {entry['number']} {entry['title']}")
 
-                sections_to_voice = list()
+                    # pad number to be 3 digits
+                    number = str(entry['number']).zfill(3)
 
-                # Retrieve the sections to voice, all those that start with "Introduction"
-                for section in entry["sections"]:
-                    if section["header"].startswith("Introduction"):
-                        section_to_voice = {
-                            "header": section["header"],
-                            "text": section["text"]
+                    # Create a folder for the scenario
+                    scenario_folder = os.path.join(output_folder, f"{number} - {entry['title']}")
+                    if not os.path.exists(scenario_folder):
+                        os.makedirs(scenario_folder)
+
+                    sections_to_voice = list()
+
+                    # Retrieve the sections to voice, all those that start with "Introduction"
+                    for section in entry["sections"]:
+                        if section["header"].startswith("Introduction"):
+                            section_to_voice = {
+                                "header": section["header"],
+                                "text": section["text"]
+                            }
+
+                            # Add it to the list of sections to voice
+                            sections_to_voice.append(section_to_voice)
+
+                    # Write the manifest file
+                    manifest_file = os.path.join(scenario_folder, "manifest.json")
+
+                    # Check if the manifest file already exists, if it does, read it and update it with the new manifest, otherwise create a new manifest file
+                    if os.path.exists(manifest_file):
+                        with open(manifest_file, "r", encoding="utf-8") as f:
+                            manifest = json.load(f)
+                    else:
+                        manifest = {
+                            "scenario": {
+                                "number": number,
+                                "title": entry["title"]
+                            },
+                            "clips": []
                         }
 
-                        # Add it to the list of sections to voice
-                        sections_to_voice.append(section_to_voice)
+                    clips: list[dict] = manifest["clips"]
 
-                manifest = {
-                    "scenario": {
-                        "number": number,
-                        "title": entry["title"]
-                    },
-                    "clips": []
-                }
+                    # build manifest clips once (header + shared text)
+                    for section in sections_to_voice:
 
-                clips: list[dict] = manifest["clips"]
+                        # check if the header already exists in the manifest, if it does, skip it, otherwise add it to the manifest
+                        if not any(clip["header"] == section["header"] for clip in clips):
+                            manifest["clips"].append({
+                                "header": section["header"],
+                                "text": section["text"],
+                                "audio": {}
+                            })
 
-                # build manifest clips once (header + shared text)
-                for section in sections_to_voice:
-                    manifest["clips"].append({
-                        "header": section["header"],
-                        "text": section["text"],
-                        "audio": {}
-                    })
+                    # Per voice, synthesize the audio for the sections to voice
+                    for voice in voices:
+                        # Creat the folder that is needed for the voice
+                        voice_folder = os.path.join(scenario_folder, voice["name"])
+                        if not os.path.exists(voice_folder):
+                            os.makedirs(voice_folder)
 
-                # Per voice, synthesize the audio for the sections to voice
-                for voice in voices:
-                    # Creat the folder that is needed for the voice
-                    voice_folder = os.path.join(scenario_folder, voice)
-                    if not os.path.exists(voice_folder):
-                        os.makedirs(voice_folder)
+                        for clip in clips:
+                            filename = f"{clip['header']}.wav"
+                            filename = os.path.join(voice_folder, filename)
 
-                    for clip in clips:
-                        filename = f"{clip['header']}.wav"
-                        filename = os.path.join(voice_folder, filename)
+                            #synthesize_audio(model_client, tokenizer, clip["text"], voice, filename)
 
-                        synthesize_audio(clip["text"], voice, filename)
+                            clip["audio"][voice["name"]] = f"{voice['name']}/{clip['header']}.wav"
 
-                        clip["audio"][voice] = f"{voice}/{clip['header']}.wav"
+                    with open(manifest_file, "w", encoding="utf-8") as f:
+                        json.dump(manifest, f, indent=2, ensure_ascii=False)
 
-                # Write the manifest file
-                manifest_file = os.path.join(scenario_folder, "manifest.json")
-                with open(manifest_file, "w", encoding="utf-8") as f:
-                    json.dump(manifest, f, indent=2, ensure_ascii=False)
-
+        except Exception as e:
+            print(e)
 
 if __name__ == "__main__":
     main()
